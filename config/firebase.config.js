8723cfdccbb9bd2a659fae83d704e6d631efaea4
AlexBOYERE <proalexandreboyere@gmail.com>
2023-04-19 09:57:01 +0200

    feature(firebase): mise en place

---
+++ config/firebase.config.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = initializeApp({
+    apiKey: "AIzaSyBuyi6bYrqAmAIt3QJjFxziG4NAsSnXJoY",
+    authDomain: "quizzcesi-8cc52.firebaseapp.com",
+    projectId: "quizzcesi-8cc52",
+    storageBucket: "quizzcesi-8cc52.appspot.com",
+    messagingSenderId: "102481079546",
+    appId: "1:102481079546:web:91b36df8658f5cf63a2d43"
+});
